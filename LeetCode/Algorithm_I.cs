aaf031e2ff53f39dda3c85cec88e20e306538e89
thjo <fortunejth@gmail.com>
2021-10-26 21:09:56 -0400

    Algorithm - two points 2

--- LeetCode/Algorithm_I.cs
+++ LeetCode/Algorithm_I.cs
@@ -160,6 +160,102 @@ namespace LeetCode
             return newSorted;
         }
 
+
+        /// <summary>
+        /// 189. Rotate Array
+        /// https://leetcode.com/problems/rotate-array/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        public void Rotate(int[] nums, int k)
+        {
+            if (nums.Length == 1)
+                return;
+
+            k = k % nums.Length;
+
+            ReversalArray(nums, 0, nums.Length-1);
+            ReversalArray(nums, 0, k-1);
+            ReversalArray(nums, k, nums.Length-1);
+
+        }
+        private void ReversalArray(int[] nums, int start, int end)
+        {
+            while(start < end)
+            {
+                int tmp = nums[start];
+                nums[start++] = nums[end];
+                nums[end--] = tmp;
+            }
+        }
+
+
+        /// <summary>
+        /// 283. Move Zeroes
+        /// https://leetcode.com/problems/move-zeroes/
+        /// </summary>
+        /// <param name="nums"></param>
+        public void MoveZeroes(int[] nums) 
+        {   
+            int numOfZeros = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                    numOfZeros++;
+                else
+                {
+                    if (numOfZeros > 0)
+                    {
+                        nums[i - numOfZeros] = nums[i];
+                        nums[i] = 0;
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 167. Two Sum II - Input array is sorted
+        /// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            //Using two points
+            int idxLeft = 0, idxRight = numbers.Length - 1;
+
+            while(idxLeft < idxRight)
+            {
+                if (numbers[idxLeft] + numbers[idxRight] == target)
+                    return new int[] { idxLeft, idxRight };
+                else if (numbers[idxLeft] + numbers[idxRight] > target)
+                    idxRight--;
+                else
+                    idxLeft++;
+            }
+            return new int[] { -1, -1 };
+            //Using Dictionary
+
+            //Dictionary<int, int> numMap = new Dictionary<int, int>();
+            //for(int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numMap.ContainsKey(target - numbers[i]))
+            //    {
+            //        return new int[] { numMap[target - numbers[i]], i + 1 };
+            //    }
+            //    else
+            //    {
+            //        if(numMap.ContainsKey(numbers[i]) == false)
+            //            numMap.Add(numbers[i], i + 1);
+            //    }
+            //}
+
+            //return new int[] { -1, -1 };
+        }
+
         #endregion
     }
 }
