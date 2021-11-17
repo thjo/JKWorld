e2a4717bc223eba6a782151157bb7d27ab527312
thjo <fortunejth@gmail.com>
2021-11-16 21:45:10 -0500

    Recursion / Backtracking

--- LeetCode/Algorithm_I.cs
+++ LeetCode/Algorithm_I.cs
@@ -23,9 +23,9 @@ namespace LeetCode
             int left = 0, right = len - 1;
             int mid = len / 2;
 
-            while(left <= right)
+            while (left <= right)
             {
-                if(nums[mid] == target)
+                if (nums[mid] == target)
                 {
                     targetIdx = mid;
                     break;
@@ -129,7 +129,7 @@ namespace LeetCode
 
             int[] newSorted = new int[len];
             int idxNew = 0;
-            for(int i = 0; i < nums.Length; i++)
+            for (int i = 0; i < nums.Length; i++)
             {
                 if (nums[i] >= 0)
                 {
@@ -140,7 +140,7 @@ namespace LeetCode
                         newSorted[idxNew++] = nums[negativPoint] * nums[negativPoint];
                         negativPoint--;
                     }
-                    newSorted[idxNew] = nums[i] * nums[i]; 
+                    newSorted[idxNew] = nums[i] * nums[i];
                     idxNew++;
                 }
                 else
@@ -174,14 +174,14 @@ namespace LeetCode
 
             k = k % nums.Length;
 
-            ReversalArray(nums, 0, nums.Length-1);
-            ReversalArray(nums, 0, k-1);
-            ReversalArray(nums, k, nums.Length-1);
+            ReversalArray(nums, 0, nums.Length - 1);
+            ReversalArray(nums, 0, k - 1);
+            ReversalArray(nums, k, nums.Length - 1);
 
         }
         private void ReversalArray(int[] nums, int start, int end)
         {
-            while(start < end)
+            while (start < end)
             {
                 int tmp = nums[start];
                 nums[start++] = nums[end];
@@ -195,11 +195,11 @@ namespace LeetCode
         /// https://leetcode.com/problems/move-zeroes/
         /// </summary>
         /// <param name="nums"></param>
-        public void MoveZeroes(int[] nums) 
-        {   
+        public void MoveZeroes(int[] nums)
+        {
             int numOfZeros = 0;
 
-            for(int i = 0; i < nums.Length; i++)
+            for (int i = 0; i < nums.Length; i++)
             {
                 if (nums[i] == 0)
                     numOfZeros++;
@@ -227,10 +227,10 @@ namespace LeetCode
             //Using two points
             int idxLeft = 0, idxRight = numbers.Length - 1;
 
-            while(idxLeft < idxRight)
+            while (idxLeft < idxRight)
             {
                 if (numbers[idxLeft] + numbers[idxRight] == target)
-                    return new int[] { idxLeft+1, idxRight+1 };
+                    return new int[] { idxLeft + 1, idxRight + 1 };
                 else if (numbers[idxLeft] + numbers[idxRight] > target)
                     idxRight--;
                 else
@@ -288,17 +288,17 @@ namespace LeetCode
             string subStr = "";
             for (int i = 0; i < s.Length; i++)
             {
-                if(s[i] == ' ')
+                if (s[i] == ' ')
                 {
                     reverseStr += string.Format("{0} ", ReverseString(subStr));
                     subStr = "";
                 }
                 else
                 {
-                    subStr += s[i]; 
+                    subStr += s[i];
                 }
             }
-            if(string.IsNullOrWhiteSpace(subStr) == false)
+            if (string.IsNullOrWhiteSpace(subStr) == false)
                 reverseStr += string.Format("{0}", ReverseString(subStr));
 
             return reverseStr;
@@ -427,7 +427,7 @@ namespace LeetCode
                 if (subStr.ContainsKey(s[currIdx]))
                 {
                     int duplicatedIdx = subStr[s[currIdx]];
-                    while ( startIdx <= duplicatedIdx)
+                    while (startIdx <= duplicatedIdx)
                     {
                         if (subStr.ContainsKey(s[startIdx]))
                         {
@@ -436,7 +436,7 @@ namespace LeetCode
                         startIdx++;
                     }
                 }
-                
+
                 subStr.Add(s[currIdx], currIdx);
                 longestLen = Math.Max(longestLen, subStr.Count);
             }
@@ -462,7 +462,7 @@ namespace LeetCode
             if (s2.Length == 0 || s1Len > s2Len)
                 return false;
 
-            int[] s1Arr = new int[26]; 
+            int[] s1Arr = new int[26];
             int[] s2Arr = new int[26];
 
             for (int i = 0; i < s1Len; i++)
@@ -471,7 +471,7 @@ namespace LeetCode
                 s2Arr[s2[i] - 'a']++;
             }
 
-            for(int i = 0; i < s2Len- s1Len; i++)
+            for (int i = 0; i < s2Len - s1Len; i++)
             {
                 if (isPermutation(s1Arr, s2Arr))
                     return true;
@@ -487,7 +487,7 @@ namespace LeetCode
         }
         private bool isPermutation(int[] s1Arr, int[] s2Arr)
         {
-            for(int i = 0; i < s1Arr.Length; i++)
+            for (int i = 0; i < s1Arr.Length; i++)
             {
                 if (s1Arr[i] != s2Arr[i])
                     return false;
@@ -533,10 +533,10 @@ namespace LeetCode
             {
                 image[sr][sc] = newColor;
                 visited[sr][sc] = true;
-                FloodFillRecursive(image, sr-1, sc, rowLen, colLen, visited, oldColor, newColor);
-                FloodFillRecursive(image, sr, sc-1, rowLen, colLen, visited, oldColor, newColor);
-                FloodFillRecursive(image, sr+1, sc, rowLen, colLen, visited, oldColor, newColor);
-                FloodFillRecursive(image, sr, sc+1, rowLen, colLen, visited, oldColor, newColor);
+                FloodFillRecursive(image, sr - 1, sc, rowLen, colLen, visited, oldColor, newColor);
+                FloodFillRecursive(image, sr, sc - 1, rowLen, colLen, visited, oldColor, newColor);
+                FloodFillRecursive(image, sr + 1, sc, rowLen, colLen, visited, oldColor, newColor);
+                FloodFillRecursive(image, sr, sc + 1, rowLen, colLen, visited, oldColor, newColor);
             }
         }
 
@@ -556,11 +556,11 @@ namespace LeetCode
             for (int i = 0; i < rowLen; i++)
                 visited[i] = new int[colLen];
 
-            for(int r = 0; r < rowLen; r++)
+            for (int r = 0; r < rowLen; r++)
             {
-                for(int c = 0; c < colLen; c++)
+                for (int c = 0; c < colLen; c++)
                 {
-                    if(grid[r][c] == 1)
+                    if (grid[r][c] == 1)
                     {
                         maxAreaOfIsland = Math.Max(VisitIsland(grid, r, c, rowLen, colLen), maxAreaOfIsland);
                     }
@@ -579,10 +579,10 @@ namespace LeetCode
 
             grid[pointRow][pointCol] = 2;       //Visied
             sizeOfIsland++;
-            sizeOfIsland += VisitIsland(grid, pointRow-1, pointCol, rowLen, colLen);
-            sizeOfIsland += VisitIsland(grid, pointRow, pointCol-1, rowLen, colLen);
-            sizeOfIsland += VisitIsland(grid, pointRow, pointCol+1, rowLen, colLen);
-            sizeOfIsland += VisitIsland(grid, pointRow+1, pointCol, rowLen, colLen);
+            sizeOfIsland += VisitIsland(grid, pointRow - 1, pointCol, rowLen, colLen);
+            sizeOfIsland += VisitIsland(grid, pointRow, pointCol - 1, rowLen, colLen);
+            sizeOfIsland += VisitIsland(grid, pointRow, pointCol + 1, rowLen, colLen);
+            sizeOfIsland += VisitIsland(grid, pointRow + 1, pointCol, rowLen, colLen);
 
             return sizeOfIsland;
         }
@@ -618,13 +618,244 @@ namespace LeetCode
             return newTree;
         }
 
+        /// <summary>
+        /// 116. Populating Next Right Pointers in Each Node
+        /// https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public Node ConnectI(Node root)
+        {
+            Node leftNode = root;
+
+            while (leftNode != null && leftNode.left != null)
+            {
+                ConnectNextNodes(leftNode);
+                leftNode = leftNode.left;
+            }
+            return root;
+        }
+        private void ConnectNextNodes(Node node)
+        {
+            while (node != null)
+            {
+                node.left.next = node.right;
+                if (node.next != null)
+                    node.right.next = node.next.left;
+                node = node.next;
+            }
+        }
 
 
+        /// <summary>
+        /// 542. 01 Matrix
+        /// https://leetcode.com/problems/01-matrix/
+        /// </summary>
+        /// <param name="mat"></param>
+        /// <returns></returns>
+        public int[][] UpdateMatrix(int[][] mat)
+        {
+            int lenRows = mat.Length;
+            int lenCols = mat[0].Length;
+            int[][] dist = new int[lenRows][];
+            for (int row = 0; row < lenRows; row++)
+                dist[row] = new int[lenCols];
+
+            for (int row = 0; row < lenRows; row++)
+            {
+                for (int col = 0; col < lenCols; col++)
+                {
+                    dist[row][col] = UpdateMatrixHelper(mat, lenRows, lenCols, row, col, dist);
+                }
+            }
+
+            return dist;
+        }
+        public int UpdateMatrixHelper(int[][] mat, int lenRows, int lenCols, int iRow, int iCol, int[][] dist)
+        {
+            if (dist[iRow][iCol] != 0)
+                return dist[iRow][iCol];
+            else if (mat[iRow][iCol] == 0)
+            {
+                dist[iRow][iCol] = 0;
+                return dist[iRow][iCol];
+            }
+
+            int? minVal = null;
+
+            //Check Top
+            if (iRow - 1 >= 0 && mat[iRow - 1][iCol] == 1)
+            {
+                int tmp = UpdateMatrixHelper(mat, lenRows, lenCols, iRow - 1, iCol, dist) + 1;
+                if (minVal == null)
+                    minVal = tmp;
+                else
+                    minVal = Math.Min(minVal.Value, tmp);
+            }
+            //Right
+            if (iCol + 1 < lenCols && mat[iRow][iCol + 1] == 1)
+            {
+                int tmp = UpdateMatrixHelper(mat, lenRows, lenCols, iRow, iCol + 1, dist) + 1;
+                if (minVal == null)
+                    minVal = tmp;
+                else
+                    minVal = Math.Min(minVal.Value, tmp);
+            }
+
+            //Check left
+            if (iCol - 1 >= 0 && mat[iRow][iCol - 1] == 1)
+            {
+                int tmp = UpdateMatrixHelper(mat, lenRows, lenCols, iRow, iCol - 1, dist) + 1;
+                if (minVal == null)
+                    minVal = tmp;
+                else
+                    minVal = Math.Min(minVal.Value, tmp);
+            }
+
+            //Check Bottom
+            if (iRow + 1 < lenRows && mat[iRow + 1][iCol] == 1)
+            {
+                int tmp = UpdateMatrixHelper(mat, lenRows, lenCols, iRow + 1, iCol, dist) + 1;
+                if (minVal == null)
+                    minVal = tmp;
+                else
+                    minVal = Math.Min(minVal.Value, tmp);
+            }
+
+            dist[iRow][iCol] = minVal != null ? minVal.Value : mat[iRow][iCol];
+            return dist[iRow][iCol];
+        }
+
+
+
+        /// <summary>
+        /// 994. Rotting Oranges
+        /// https://leetcode.com/problems/rotting-oranges/
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <returns></returns>
+        public int OrangesRotting(int[][] grid)
+        {
+            int[][] directions = new int[4][];
+            directions[0] = new int[] { 1, 0 };
+            directions[1] = new int[] { 0, 1 };
+            directions[2] = new int[] { -1, 0 };
+            directions[3] = new int[] { 0, -1 };
+
+            Queue<int[]> rottenOrangs = new Queue<int[]>();
+            int freshOranges = 0;
+            for (int r = 0; r < grid.Length; r++)
+            {
+                for (int c = 0; c < grid[r].Length; c++)
+                {
+                    if (grid[r][c] == 2)
+                    {
+                        List<int> loc = new List<int>();
+                        loc.Add(r); loc.Add(c);
+                        rottenOrangs.Enqueue(new int[] { r, c, 0 });
+                    }
+                    else if (grid[r][c] == 1)
+                        freshOranges++;
+                }
+            }
+
+            int numfMins = 0;
+            while (rottenOrangs.Count > 0)
+            {
+                var map = rottenOrangs.Dequeue();
+                numfMins = Math.Max(map[2], numfMins);
+                for (int i = 0; i < directions.Length; i++)
+                {
+                    int x = map[0] + directions[i][0];
+                    int y = map[1] + directions[i][1];
+                    if (grid[x][y] == 1)
+                    {
+                        grid[x][y] = 2;
+                        freshOranges--;
+                        rottenOrangs.Enqueue(new int[] { x, y, (map[2] + 1) });
+                    }
+                }
+            }
+
+            return freshOranges > 0 ? -1 : numfMins;
+        }
 
 
 
         #endregion
 
 
+
+        #region | Recursion / Backtracking | 
+
+        /// <summary>
+        /// 21. Merge Two Sorted Lists
+        /// https://leetcode.com/problems/merge-two-sorted-lists/
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 206. Reverse Linked List
+        /// https://leetcode.com/problems/reverse-linked-list/
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode reverseList(ListNode head)
+        {
+            Stack<ListNode> sList = new Stack<ListNode>();
+
+            ListNode currNode = head;
+            while (currNode != null) 
+            {
+                sList.Push(currNode);
+                currNode = currNode.next;
+            }
+
+            ListNode newHead = currNode;
+            while (sList.Count > 0)
+            {
+                currNode.next = sList.Pop();
+                currNode = currNode.next;
+            }
+
+            return newHead.next;
+        }
+
+        #endregion
+
+    }
+
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next)
+        {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+    public class Orange
+    {
+        public int numOfMins;
+        public int x, y;
     }
 }
