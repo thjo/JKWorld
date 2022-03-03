329c04e6ccbe19f8d9e020b7586b9fa38d59156e
thjo <fortunejth@gmail.com>
2022-03-02 21:48:44 -0500

    Programming Skills I - 3/02/2022

---
+++ LeetCode/ProgrammingSkills_I.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ProgrammingSkills_I
+    {
+        /// <summary>
+        /// 1523. Count Odd Numbers in an Interval Range
+        /// https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+        /// </summary>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        /// <returns></returns>
+        public int CountOdds(int low, int high)
+        {
+            int res = (high - low) / 2;
+         
+            if (high % 2 == 1 || low % 2 == 1)
+            {
+                res++;
+            }
+            
+            return res;
+        }
+
+        /// <summary>
+        /// 1491. Average Salary Excluding the Minimum and Maximum Salary
+        /// https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+        /// </summary>
+        /// <param name="salary"></param>
+        /// <returns></returns>
+        public double Average(int[] salary)
+        {
+            if (salary.Length <= 2)
+                return 0;
+
+            int min = salary[0];
+            int max = salary[0];
+            double sum = 0;
+            foreach(int s in salary)
+            {
+                sum += s;
+                min = Math.Min(min, s);
+                max = Math.Max(max, s);
+            }
+
+            return (sum - min - max) / (salary.Length - 2);
+        }
+
+
+        /// <summary>
+        /// 191. Number of 1 Bits
+        /// https://leetcode.com/problems/number-of-1-bits/
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int HammingWeight(uint n)
+        {
+            int retVal = 0;
+            while ((n | 0) > 0)
+            {
+                if ((n & 1) > 0)
+                    retVal++;
+                n = n >> 1;
+            }
+
+            return retVal;
+        }
+
+
+        /// <summary>
+        /// 1281. Subtract the Product and Sum of Digits of an Integer
+        /// https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int SubtractProductAndSum(int n)
+        {
+            int pDigits = 1;
+            int sDigits = 0; 
+
+            while(n > 0)
+            {
+                int d = n % 10;
+                pDigits *= d;
+                sDigits += d;
+                n = n / 10;
+            }
+
+            return pDigits - sDigits;
+        }
+    }
+}
