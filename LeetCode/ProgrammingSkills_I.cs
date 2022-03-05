14662502bd8f4b8b5e9f4775b0a6d539d6725cfb
thjo <fortunejth@gmail.com>
2022-03-05 15:50:09 -0500

    Programming Skill I - 3/05/2022

--- LeetCode/ProgrammingSkills_I.cs
+++ LeetCode/ProgrammingSkills_I.cs
@@ -16,12 +16,12 @@ namespace LeetCode
         public int CountOdds(int low, int high)
         {
             int res = (high - low) / 2;
-         
+
             if (high % 2 == 1 || low % 2 == 1)
             {
                 res++;
             }
-            
+
             return res;
         }
 
@@ -39,7 +39,7 @@ namespace LeetCode
             int min = salary[0];
             int max = salary[0];
             double sum = 0;
-            foreach(int s in salary)
+            foreach (int s in salary)
             {
                 sum += s;
                 min = Math.Min(min, s);
@@ -79,9 +79,9 @@ namespace LeetCode
         public int SubtractProductAndSum(int n)
         {
             int pDigits = 1;
-            int sDigits = 0; 
+            int sDigits = 0;
 
-            while(n > 0)
+            while (n > 0)
             {
                 int d = n % 10;
                 pDigits *= d;
@@ -144,6 +144,147 @@ namespace LeetCode
             }
 
             return idxMinDist;
+        }
+
+        /// <summary>
+        /// 1790. Check if One String Swap Can Make Strings Equal
+        /// https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        public bool AreAlmostEqual(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+                return false;
+
+            char? c1 = null;
+            char? c2 = null;
+            int cnt = 0;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] == s2[i])
+                    continue;
+                else
+                {
+                    cnt++;
+                    if (cnt > 2)
+                        return false;
+
+                    if (c1 == null)
+                    {
+                        c1 = s1[i];
+                        c2 = s2[i];
+                    }
+                    else
+                    {
+                        if (c1 != c2 && c1 == s2[i] && c2 == s1[i])
+                            c1 = c2;
+                    }
+                }
+            }
+
+            if (cnt == 0 || (cnt == 2 && c1.Value == c2.Value))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// 202. Happy Number
+        /// https://leetcode.com/problems/happy-number/
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public bool IsHappy(int n)
+        {
+            if (n == 1)
+                return true;
+
+            int slowRunner = n;
+            int fastRunner = getNext(n);
+            while (fastRunner != 1 && fastRunner != slowRunner)
+            {
+                slowRunner = getNext(slowRunner);
+                fastRunner = getNext(getNext(fastRunner));
+            }
+
+            if (fastRunner == 1)
+                return true;
+            else
+                return false;
+        }
+        private int getNext(int n)
+        {
+            int totalSum = 0;
+            while (n > 0)
+            {
+                int d = n % 10;
+                n = n / 10;
+                totalSum += d * d;
+            }
+
+            return totalSum;
+        }
+
+
+        /// <summary>
+        /// 1502. Can Make Arithmetic Progression From Sequence
+        /// https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public bool CanMakeArithmeticProgression(int[] arr)
+        {
+            //Brute Force
+            //Array.Sort(arr);
+            //int d = arr[1] - arr[0];
+            //for (int i = 2; i < arr.Length; i++)
+            //{
+            //    if (arr[i] - arr[i - 1] != d)
+            //        return false;
+            //}
+            //return true;
+
+            //Optimization
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            HashSet<int> set = new HashSet<int>();
+
+            foreach (int num in arr)
+            {
+                min = Math.Min(min, num);
+                max = Math.Max(max, num);
+                set.Add(num);
+            }
+            int n = arr.Length;
+            int d = (max - min);
+            if (d % (n - 1) != 0)
+                return false;
+            d /= (n - 1);
+            int i = 0;
+            while (i < n)
+            {
+                if (set.Contains(min) == false)
+                    return false;
+
+                min += d;
+                i++;
+            }
+
+            return true;
+        }
+
+
+        /// <summary>
+        /// 1822. Sign of the Product of an Array
+        /// https://leetcode.com/problems/sign-of-the-product-of-an-array/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int ArraySign(int[] nums)
+        {
+
         }
     }
 }
