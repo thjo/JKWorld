c48a8012c028dd808b6765c83ea6fcae158701b8
thjo <fortunejth@gmail.com>
2022-03-28 22:36:37 -0400

    LeetCode 76 cases I - 3/28/2022

---
+++ LeetCode/Leetcode75Questions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Leetcode75Questions
+    {
+        /// <summary>
+        /// 1. Two Sum
+        /// https://leetcode.com/problems/two-sum/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dp = new Dictionary<int, int>();
+            int[] res = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dp.ContainsKey(target - nums[i]))
+                {
+                    res[0] = dp[target - nums[i]];
+                    res[1] = i;
+                    break;
+                }
+                else
+                    dp[nums[i]] = i;
+            }
+
+            return res;
+        }
+    }
+}
