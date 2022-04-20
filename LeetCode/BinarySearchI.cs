bff63acf07adfb267317e8e4a51dbda76299ffe1
thjo <fortunejth@gmail.com>
2022-04-19 23:52:35 -0400

    Pratice - 4/19/2022

--- LeetCode/BinarySearchI.cs
+++ LeetCode/BinarySearchI.cs
@@ -29,5 +29,59 @@ namespace LeetCode
             }
             return -1;
         }
+
+
+        /// <summary>
+        /// 35. Search Insert Position
+        /// https://leetcode.com/problems/search-insert-position/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int SearchInsert(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length - 1;
+            int mid = (left + right) / 2;
+
+            while (left <= right)
+            {
+                if (nums[mid] == target)
+                    return mid;
+                else if (nums[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+
+                mid = (left + right) / 2;
+            }
+
+            return left;
+        }
+
+        /// <summary>
+        /// 852. Peak Index in a Mountain Arra
+        /// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public int PeakIndexInMountainArray(int[] arr)
+        {
+            int l = 0, h = arr.Length - 1;
+            while (l < h)
+            {
+                int m = l + (h - l) / 2;
+                if (arr[m] < arr[m + 1])
+                    l = m + 1;
+                else
+                    h = m;
+            }
+            return l;
+        }
+
+
     }
 }
